7099a43bfb13c1cca2d2a8712ab45417d08245f8
unknown <luciano11v11@gmail.com>
2020-11-13 10:44:35 -0300

    Update DATABASE

--- bd.sql
+++ bd.sql
@@ -30,9 +30,9 @@ CREATE TABLE equipes (
     data_fundacao DATE NOT NULL,
     created DATETIME,
     modified DATETIME,
-    autor INT,
+    usuario_id INT,
 
-    CONSTRAINT FK_autor_Equipe FOREIGN KEY (autor) REFERENCES usuarios(usuario_id)
+    CONSTRAINT FK_usuario_id_Equipe FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id)
 );
 
 
@@ -45,9 +45,9 @@ CREATE TABLE jogadores (
     posicao VARCHAR(255) NOT NULL,
     created DATETIME,
     modified DATETIME,
-    autor INT,
+    usuario_id INT,
 
-    CONSTRAINT FK_autor_Jogador FOREIGN KEY (autor) REFERENCES usuarios(usuario_id),
+    CONSTRAINT FK_usuario_id_Jogador FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
     CONSTRAINT FK_equipe_id_Jogador FOREIGN KEY (equipe_id) REFERENCES equipes(equipe_id)
 );
 
@@ -61,9 +61,9 @@ CREATE TABLE partidas (
     gols_casa INT NOT NULL,
     created DATETIME,
     modified DATETIME,
-    autor INT,
+    usuario_id INT,
     
-    CONSTRAINT FK_autor_Partida FOREIGN KEY (autor) REFERENCES usuarios(usuario_id),
+    CONSTRAINT FK_usuario_id_Partida FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
     CONSTRAINT FK_equipe_casa_Partida FOREIGN KEY (equipe_casa_id) REFERENCES equipes(equipe_id),
     CONSTRAINT FK_equipe_fora_Partida FOREIGN KEY (equipe_fora_id) REFERENCES equipes(equipe_id)
 );
@@ -75,25 +75,63 @@ VALUES ("Padrão", "usuario@teste.com", "password", NOW(), NOW());
 
 
 
-INSERT INTO equipes(nome, data_fundacao, created, modified, autor) 
+INSERT INTO equipes(nome, data_fundacao, created, modified, usuario_id) 
 VALUES ("Goiás", "1980-10-10", NOW(), NOW(), 1),
     ("Vila Nova", "1981-10-10", NOW(), NOW(), 1);
 
 
 
-INSERT INTO jogadores(equipe_id, nome, sobrenome, idade, posicao, created, modified, autor) 
+INSERT INTO jogadores(equipe_id, nome, sobrenome, idade, posicao, created, modified, usuario_id) 
 VALUES (1, "Fulano", "da Silva", "35", "Goleiro", NOW(), NOW(), 1),
     (2, "Ciclano", "Souza", "33", "Zagueiro", NOW(), NOW(), 1);
 
 
 
-INSERT INTO partidas(equipe_casa_id, equipe_fora_id, data_partida, gols_casa, gols_fora, created, modified, autor)
+INSERT INTO partidas(equipe_casa_id, equipe_fora_id, data_partida, gols_casa, gols_fora, created, modified, usuario_id)
 VALUES (2, 1, NOW(), 2, 2, NOW(), NOW(), 1);
 
 
+/*
+SELECT
+   Jogadores.jogador_id AS `Jogadores__jogador_id`,
+   Jogadores.equipe_id AS `Jogadores__equipe_id`,
+   Jogadores.nome AS `Jogadores__nome`,
+   Jogadores.sobrenome AS `Jogadores__sobrenome`,
+   Jogadores.idade AS `Jogadores__idade`,
+   Jogadores.posicao AS `Jogadores__posicao`,
+   Jogadores.created AS `Jogadores__created`,
+   Jogadores.modified AS `Jogadores__modified`,
+   Jogadores.autor AS `Jogadores__autor`,
+   Equipes.equipe_id AS `Equipes__equipe_id`,
+   Equipes.nome AS `Equipes__nome`,
+   Equipes.data_fundacao AS `Equipes__data_fundacao`,
+   Equipes.created AS `Equipes__created`,
+   Equipes.modified AS `Equipes__modified`,
+   Equipes.autor AS `Equipes__autor`,
+   Usuarios.usuario_id AS `Usuarios__usuario_id`,
+   Usuarios.nome_de_usuario AS `Usuarios__nome_de_usuario`,
+   Usuarios.email AS `Usuarios__email`,
+   Usuarios.senha AS `Usuarios__senha`,
+   Usuarios.created AS `Usuarios__created`,
+   Usuarios.modified AS `Usuarios__modified` 
+FROM
+   jogadores Jogadores 
+   INNER JOIN
+      equipes Equipes 
+      ON Equipes.equipe_id = 
+      (
+         Jogadores.equipe_id
+      )
+   INNER JOIN
+      usuarios Usuarios 
+      ON Usuarios.usuario_id = 
+      (
+         Jogadores.usuario_id
+      )
+      LIMIT 20 OFFSET 0
 
 
-
+*/
 /*
 SELECT eq.nome AS Equipe_da_Casa, eq2.nome AS Equipe_de_Fora, pt.data_partida
 FROM partidas pt 
