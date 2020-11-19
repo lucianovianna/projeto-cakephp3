db2a29ea24d9ed16a8bf3ee7b3672677ff01e47e
lucianovianna <luciano11v11@gmail.com>
2020-11-19 14:04:42 -0300

    Creating ExportsController

---
+++ Partidas/src/Controller/ExportsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+use Cake\Datasource\ConnectionManager;
+
+
+class ExportsController extends AppController 
+{
+    public function report1() {
+
+    }
+
+    public function report2() {
+
+    }
+
+    public function report3() {
+        
+    }
+}
